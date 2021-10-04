1cfc4f5eeb424c436c6cf8e546e2ae26c0107e9c
Aleksandr Vasilev <sovietxyouth@gmail.com>
2021-10-04 21:51:58 +0300

    2nd hw

---
+++ HW2/1.js
@@ -0,0 +1,56 @@
+function makeObjectDeepCopy(obj) {
+  if (typeof obj !== "object" || obj === null) {
+    return obj 
+  }
+  let newObj = Array.isArray(obj) ? [] : {}
+  for (let key in obj) {
+    let value = obj[key]
+    newObj[key] = makeObjectDeepCopy(value)
+  }
+  return newObj
+} 
+
+function selectFromInterval(array, firstValue, secondValue) { 
+  let newArr = [] 
+  if (!Array.isArray(array) || (array.some(el => !Number.isInteger(el)))) { 
+    throw new Error('Ошибка!') 
+  } 
+  if ([firstValue,secondValue].some(el => !Number.isInteger(el))) {
+    throw new Error('Ошибка!') 
+  }
+  if (firstValue < secondValue) { 
+    newArr = array.filter(el => el >= firstValue && el <= secondValue) 
+  } else { 
+    newArr = array.filter(el => el >= secondValue && el <= firstValue) 
+  } 
+  return newArr 
+} 
+  
+
+const myIterable = {
+  from: 7,
+  to: 5,
+
+  [Symbol.iterator]() {
+    if (this.to < this.from){
+      throw new Error('Ошибка!') 
+    }
+    if ([this.from, this.to].some(el => !Number.isInteger(el))){
+      throw new Error('Ошибка!') 
+    }
+    this.from;
+    return this;
+  },
+
+  next() {
+    if (this.from <= this.to) {
+      return { done: false, value: this.from++ };
+  
+    } else {
+      return { done: true };
+    }
+  }
+};
+
+
+
